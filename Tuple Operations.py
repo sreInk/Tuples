75730e73d631ccda23b0419e0002931708316a8f
sreInk <srinknok@gmail.com>
2024-10-16 09:11:50 +0530

    w1

---
+++ Tuple Operations.py
@@ -0,0 +1,17 @@
+t = "w",False ,32.1,65
+print(t)
+
+t = (4,343,1.32,3,2,4.323)
+print(t)
+
+t = t + (9,)
+print(t)
+
+t1 = (50,10,200,300)
+print(t1.count(10))
+
+_slice = t[3:5] 
+print (_slice)
+
+_slice = t[:6] 
+print (_slice)
